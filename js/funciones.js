9eda22ed4d8fffc01eb3c4745ff07354e9b2a5cc
francisco-solis99 <franciscosoliscss@gmail.com>
2022-08-25 21:47:40 -0500

    [Add] the information from the API

--- js/funciones.js
+++ js/funciones.js
@@ -8,15 +8,28 @@ function getCharacters(){
 }
 
 function createCharacter(character){
+  console.log(character);
   return `
-
+      <div class="card" style="width: 8rem; height:auto">
+        <img class="card-img-top" src="${character.image}" alt="${character.name}">
+        <div class="card-body">
+          <h4 class="card-title">${character.name}</h4>
+          <p class="card-text">${character.species}</p>
+        </div>
+      </div>
   `
 }
 
 async function renderCharacters ()  {
   const data = await getCharacters();
   const characters = data.results;
-  console.log(characters);
+
+  // filter
+
+
+  const container =  document.querySelector('.cards__container');
+  const htmlCharacters = characters.map(character => createCharacter(character));
+  container.innerHTML = htmlCharacters.join('');
 }
 
 renderCharacters()
