a521e4e5cde783c65ff43758628718d6371dc8ca
francisco-solis99 <franciscosoliscss@gmail.com>
2022-08-25 21:58:20 -0500

    [Add] the  filter to get the characters in the input field

--- js/funciones.js
+++ js/funciones.js
@@ -3,12 +3,23 @@ const API = {
   characters: 'https://rickandmortyapi.com/api/character'
 }
 
+// DOM elemenrs
+const input = document.querySelector('#buscador');
+const container =  document.querySelector('.cards__container');
+
+
+input.addEventListener('input', (e) => {
+  const currentText = e.target.value.toLowerCase();
+  renderFilterCharacters(currentText);
+})
+
+
+
 function getCharacters(){
   return fetch(API.characters).then(response => response.json());
 }
 
 function createCharacter(character){
-  console.log(character);
   return `
       <div class="card" style="width: 8rem; height:auto">
         <img class="card-img-top" src="${character.image}" alt="${character.name}">
@@ -20,16 +31,16 @@ function createCharacter(character){
   `
 }
 
-async function renderCharacters ()  {
+async function renderFilterCharacters(text)  {
+
+  container.innerHTML = '';
   const data = await getCharacters();
   const characters = data.results;
 
-  // filter
-
-
-  const container =  document.querySelector('.cards__container');
-  const htmlCharacters = characters.map(character => createCharacter(character));
+  const filterCharacters = characters.filter(character => character.name.toLowerCase().includes(text));
+  const htmlCharacters = filterCharacters.map(character => createCharacter(character));
   container.innerHTML = htmlCharacters.join('');
 }
 
-renderCharacters()
+
+
